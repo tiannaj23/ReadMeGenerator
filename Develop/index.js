78e01512677a23eba5434519b542e41d7e60b7b4
tiannaj23 <tiannaj23@gmail.com>
2023-02-27 13:56:36 -0500

    connected inquirer data with the markdown file

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,81 @@
 // TODO: Include packages needed for this application
+const fs = require('fs');
+const path = require('path');
+const inquirer =require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: "What's the title of your application?"
+    },
+
+    {
+        type: 'input',
+        name: 'description',
+        message: "How would you describe this application?"
+    },
+
+    {
+        type: 'input',
+        name: 'installation',
+        message: "What are the steps required to install your project?"
+    },
+
+    {
+        type: 'input',
+        name: 'usage',
+        message: `How is this application used? Include screenshots as needed. To add a screenshot, create an assets/images folder in your repository and upload your screenshot to it. Then, using the relative filepath, add it to your README using the following syntax 
+
+        md
+        ![alt text](assets/images/screenshot.png)
+        `
+    },
+
+    {
+        type: 'input',
+        name: 'contributing',
+        message: "List any contributors to the application."
+    },
+
+    {
+        type: 'input',
+        name: 'email',
+        message: "Enter your email:"
+    },
+
+    {
+        type: 'input',
+        name: githu
+    }
+
+
+
+    {
+        type: 'list',
+        name: 'license',
+        message: "What's the app license under?",
+        choices: ['MIT', 'APACHE 2.0', 'GPL 3.0', 'BSD 3', 'None']
+    }
+
+    
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    return fs.writeFileSync(path.join(fileName), data)
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions).then((answers)=>{
+        console.log("Generating ReadMe")
+        writeToFile("README.md", generateMarkdown({...answers}))
+    })
+
+}
 
 // Function call to initialize app
 init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,18 +1,46 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if(license !== "None"){
+    return `![GitHub license](https://img.shields.io/badge/license-${license}-blue.svg`
+  }
+  return ""
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if(license !=="None") {
+    return `\n* [License](#license)\n`
+  }
+  return ""
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if(license !=="None") {
+    return `This project is licensed by ${license}`
+  }
+  return ""
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+  ${
+    renderLicenseBadge(data.license)
+  }
+
+## Description
+  
+
+## Table of Contents
+#[Title](#Title)
+${renderLicenseLink(data.license)}
+
+
+
 
 `;
 }
