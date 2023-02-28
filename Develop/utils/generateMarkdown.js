a6f3f14800b108ab9775a6a506639fbd366b0653
tiannaj23 <tiannaj23@gmail.com>
2023-02-28 17:23:39 -0500

    fixed function renderLicenseBadge

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -18,9 +18,9 @@ function renderLicenseLink(license) {
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {
-  if(license !=="None") {
-    return `This project is licensed by ${license}`
+function renderLicenseBadge(license) {
+  if(license !== "None"){
+    return `![License](https://img.shields.io/badge/License-${license}-blue.svg)`
   }
   return ""
 }
@@ -36,12 +36,12 @@ function generateMarkdown(data) {
 ${data.description}
 
 ## Table of Contents
-[Title](#title)
-[Installation](#installation)
-[Usage](#usage)
-[Tests](#tests)
-[Contributing](#contributing)
-[Questions](#questions)
+- [Title](#title)
+- [Installation](#installation)
+- [Usage](#usage)
+- [Tests](#tests)
+- [Contributing](#contributing)
+- [Questions](#questions)
 ${renderLicenseLink(data.license)}
 
 ## Installation
